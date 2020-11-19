84f54ff78d2f2707ce5aa4423f3abbceb0895890
DMUGNOLO <islaedicion@iMac-de-IslaEdicion.local>
2020-11-19 19:09:16 -0300

    Agregado de funciones en products controller

--- site/controllers/productController.js
+++ site/controllers/productController.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const { get } = require('../routes/products');
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+
+//**** funciones *****/
+
+function getAllProducts(){
+    const products = fs.readFileSync(productsFilePath, 'utf-8');
+    const productsParsed = JSON.parse(products);
+    return productsParsed;
+}
+
+function writeProducts(arrayToTransform){
+    const products = getAllProducts();
+    const productToWrite = [...products, arrayToTransform];
+    const productsJson = JSON.stringify(productToWrite, null, " ");
+    fs.writeFileSync(productsFilePath, productsJson);
+}
+
+function generateNewId(){
+    const products = getAllProducts();
+    if(products == ""){
+        return 0;
+    }
+    return products.pop().id +1;
+}
 
 
 const productController = {
