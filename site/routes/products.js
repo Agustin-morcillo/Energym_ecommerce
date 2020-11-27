7c500dded53c54f6f21f260cfce10a3583c40a4d
Agustin-morcillo <morcilloagustin95@gmail.com>
2020-11-26 22:16:51 -0300

    ajustes-rutas

--- site/routes/products.js
+++ site/routes/products.js
@@ -6,13 +6,12 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, __dirname + '/../../public/images/products/')
+        cb(null, __dirname + '/../public/images/products/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname)
     }
 })
-
 
 const upload = multer({ storage: storage })
 
