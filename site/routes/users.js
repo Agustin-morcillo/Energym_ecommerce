78bf2c8d7ee083c2224edfc4522ebf4a211f3784
Agustin-morcillo <morcilloagustin95@gmail.com>
2020-12-15 18:04:43 -0300

    avatar-adjustments

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const usersController= require("../controllers/usersController")
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/../public/images/users/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 /* GET users listing. */
 router.get("/login", usersController.login);
 router.get("/register", usersController.register)
-router.post("/register", usersController.createUser);
+router.post("/register", upload.any(),usersController.createUser);
 
 module.exports = router;
