e61f1d7838c89d3d1b1e73ea1b5219f5a26d9c19
Agustin-morcillo <morcilloagustin95@gmail.com>
2020-12-22 13:25:53 -0300

    ajustes-22/12

--- site/middlewares/validator.js
+++ site/middlewares/validator.js
@@ -8,6 +8,9 @@ const bcrypt = require("bcryptjs")
 const validator = {
     login: [
         body('email')
+            .notEmpty()
+            .withMessage("Debes completar el campo: Email")
+            .bail()
             .custom((value, { req }) => {
                 const password = req.body.password;
                 const users = allFunctions.getAllusers();
@@ -21,49 +24,52 @@ const validator = {
     ],
     register: [
         body('name')
-        .notEmpty()
-        .withMessage('Debe completar el campo: Nombre'),
+            .notEmpty()
+            .withMessage('Debe completar el campo: Nombre'),
         body('lastName')
-        .notEmpty()
-        .withMessage('Debe completar el campo: Apellido'),
+            .notEmpty()
+            .withMessage('Debe completar el campo: Apellido'),
         body('email')
-        .notEmpty()
-        .withMessage('Debe completar el campo: Email')
-        .bail()
-        .isEmail()
-        .withMessage('El email ingresado no es válido')
-        .bail()
-        .custom((value)=> {
-            const allUsers = allFunctions.getAllusers();
-            const searchUser = allUsers.find((user) => (value == user.email))
-    
-            return !searchUser;
-        })
-        .withMessage('El usuario ya existe'),
-        body('retypeEmail')
-        .custom((value, {req})=> {
-            return(value == req.body.email);
-        })
-        .withMessage ('Los email no coinciden'),
+            .notEmpty()
+            .withMessage('Debe completar el campo: Email')
+            .bail()
+            .isEmail()
+            .withMessage('El email ingresado no es válido')
+            .bail()
+            .custom((value, {req})=> {
+                return(value == req.body.retypeEmail);
+            })
+            .withMessage ('Los emails no coinciden')
+            .bail()
+            .custom((value)=> {
+                const allUsers = allFunctions.getAllusers();
+                const searchUser = allUsers.find((user) => (value == user.email))
+        
+                return !searchUser;
+            })
+            .withMessage('El email ingresado se encuentra en uso'),
         body ('password')
-        .isLength ({min:5})
-        .withMessage ('La contraseña debe tener al menos 5 caracteres'),
-        body('retype')
-        .custom ((value, {req})=> {
-            return(value == req.body.password);
-        })
-        .withMessage ('Las contraseñas no coinciden'),
+            .notEmpty()
+            .withMessage('Debe completar el campo: Contraseña')
+            .bail()
+            .custom ((value, {req})=> {
+                return(value == req.body.retype);
+            })
+            .withMessage ('Las contraseñas no coinciden')
+            .bail()
+            .isLength ({min:5})
+            .withMessage ('La contraseña debe tener al menos 5 caracteres'),
         body('avatar')
-        .custom ((value , {req}) => {
-            if(req.files[0])
-            {
-                const imageFormats = ['.jpg', '.png', '.jpeg'];
-                const userImage = path.extname (req.files[0].originalname)
-                return (imageFormats.includes(userImage));
-            }
-            return true;
-        })
-        .withMessage ("Formato de imagen Inválido. Formatos válidos: '.jpg', '.png', '.jpeg'"),
+            .custom ((value , {req}) => {
+                if(req.files[0])
+                {
+                    const imageFormats = ['.jpg', '.png', '.jpeg'];
+                    const userImage = path.extname (req.files[0].originalname)
+                    return (imageFormats.includes(userImage));
+                }
+                return true;
+            })
+            .withMessage ("Formato de imagen Inválido. Formatos válidos: '.jpg', '.png', '.jpeg'"),
     ]
 }
 
