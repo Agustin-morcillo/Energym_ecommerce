c4e9a33d324461f2008be67dd41686b2861b1bc3
DMUGNOLO <islaedicion@iMac-de-IslaEdicion.local>
2021-01-30 01:59:06 -0300

    Creacion Validacion Front Rutines Create y Edit

--- site/public/javascript/validatorRutines.js
+++ site/public/javascript/validatorRutines.js
@@ -10,35 +10,61 @@
 let form = document.querySelector("form");
 let inputs = document.querySelectorAll(".input-validator");
 let image = document.querySelector("#add-edit-product-image");
-let name = document.querySelector("#add-edit-product-name");
+let description = document.querySelector("#add-edit-product-description");
 let error = (campo, label)=>{return document.querySelector(`.create-rutine-error-${campo} ${label}`)};
 let backEndError = document.querySelector(".validation-error-product");
 
 //Estados para preventDefault----------------------------------------------------
 let estado = {
-    name:false,
-    description: false,
+    name:true,
+    price:true,
+    introduction:true,
+    description:true,
+    duration:true,
     image:true,
 }
 
 //Expresiones--------------------------------------------------------------------
 const expresiones = {
     nombre: /^[a-zA-ZÀ-ÿ\s]{5,}$/, // Letras y espacios, pueden llevar acentos.
-    image: /(.jpg|.jpeg|.png|.gif)$/i
+    image: /(.jpg|.jpeg|.png|.gif)$/i,
+    precio: /(^(?:-\d))/g,
 }
 
 //FUNCIONES DE VALIDADORES//
 
-//Distribuidor name y description------------------------------------------------
+//Distribuidor-------------------------------------------------------------------
 let validator = (inputElement, event)=>{
     if(inputElement.name == "name"){
         /*1- Empty*/ emptyValidator(inputElement); 
-        if(event.type == "keyup"){
+        if(inputElement.value != ""){
             /*2- Expresiones name*/ validExpName(inputElement, expresiones.nombre);
         }
     }
+    if(inputElement.name == "price"){
+        /*1- Empty*/ emptyValidator(inputElement); 
+        if(inputElement.value != ""){
+            /*6- Extension precio*/ extPrice(inputElement, expresiones.precio);
+        }
+    }
+    if(inputElement.name == "introduction"){
+        console.log(inputElement.name)
+        /*1- Empty*/ emptyValidator(inputElement); 
+        if(inputElement.value != ""){
+            /*7- Extension introduccion*/ extIntroduction(inputElement);
+        }
+    }
     if(inputElement.name == "description"){
-        /*5- Extension description*/ extDescription(inputElement);
+        /*9- Empty description*/ blankDescription(inputElement); 
+        if(inputElement.value != ""){
+            /*5- Extension description*/ extDescription(inputElement);
+        }
+    }
+    if(inputElement.name == "duration"){
+        /*1- Empty*/ emptyValidator(inputElement); 
+        if(inputElement.value != ""){
+            /*8- Validar mayor a 0*/ positiveNumber(inputElement);
+        }
     }
 }
 
@@ -48,13 +74,14 @@ let emptyValidator = (inputElement)=>{
         //add error
         error(inputElement.name, "small").classList.remove("front-blank-error-inactive")
         error(inputElement.name, "small").classList.add("front-blank-error-active")
-        error(inputElement.name, "input").classList.add("wrong-input")
         error(inputElement.name, "label").classList.add("wrong-label")
+        error(inputElement.name, "input").classList.add("wrong-input")
         error(inputElement.name, "p").classList.add("front-error-inactive")
         error(inputElement.name, "p").classList.remove("front-error-active")
         backEndError.classList.add("front-blank-error-inactive")
         estado[inputElement.name]= false;
-        return; 
+        console.table(estado)
+        return;
     }
     //remove error
     error(inputElement.name, "small").classList.add("front-blank-error-inactive")
@@ -134,6 +161,7 @@ let blankSubmitValidator = (inputElement)=>{
         error(inputElement.name, "input").classList.add("wrong-input")
         error(inputElement.name, "label").classList.add("wrong-label")
         backEndError.classList.add("front-blank-error-inactive")
+        error(inputElement.name, "p").classList.add("front-error-inactive")
         estado[inputElement.name]= false;
         return; 
     } else {
@@ -146,9 +174,96 @@ let blankSubmitValidator = (inputElement)=>{
     }
 }
 
+//Validador 6 extension precio------------------------------------------------
+let extPrice = (inputElement, expresion)=>{
+    if(expresion.test(inputElement.value)){
+        //add error
+        error(inputElement.name, "p").classList.remove("front-error-inactive")
+        error(inputElement.name, "p").classList.add("front-error-active")
+        error(inputElement.name, "input").classList.add("wrong-input")
+        error(inputElement.name, "label").classList.add("wrong-label")
+        backEndError.classList.add("front-blank-error-inactive")
+        estado[inputElement.name]= false;
+        console.table(estado)
+        return; 
+    }
+    //remove error
+    error(inputElement.name, "p").classList.add("front-error-inactive")
+    error(inputElement.name, "p").classList.remove("front-error-active")
+    error(inputElement.name, "input").classList.remove("wrong-input")
+    error(inputElement.name, "label").classList.remove("wrong-label")
+    estado[inputElement.name]= true;
+}
+
+//Validador 7 extension introduccion-----------------------------------------
+let extIntroduction = (inputElement)=>{
+    console.log(inputElement.value.length)
+    if(inputElement.value.length < 5 || inputElement.value.length > 78){
+        //add error
+        error(inputElement.name, "p").classList.remove("front-error-inactive")
+        error(inputElement.name, "p").classList.add("front-error-active")
+        error(inputElement.name, "input").classList.add("wrong-input")
+        error(inputElement.name, "label").classList.add("wrong-label")
+        backEndError.classList.add("front-blank-error-inactive")
+        estado[inputElement.name]= false;
+        return; 
+    }
+    //remove error
+    error(inputElement.name, "p").classList.add("front-error-inactive")
+    error(inputElement.name, "p").classList.remove("front-error-active")
+    error(inputElement.name, "input").classList.remove("wrong-input")
+    error(inputElement.name, "label").classList.remove("wrong-label")
+    estado[inputElement.name]= true;
+    console.log(estado.name)
+}
+
+//Validador 8 numero mayor a 0----------------------------------------------
+let positiveNumber = (inputElement)=>{
+    console.log(inputElement.value)
+    if(inputElement.value < 1){
+        //add error
+        error(inputElement.name, "p").classList.remove("front-error-inactive")
+        error(inputElement.name, "p").classList.add("front-error-active")
+        error(inputElement.name, "input").classList.add("wrong-input")
+        error(inputElement.name, "label").classList.add("wrong-label")
+        backEndError.classList.add("front-blank-error-inactive")
+        estado[inputElement.name]= false;
+        return; 
+    }
+     //remove error
+     error(inputElement.name, "p").classList.add("front-error-inactive")
+     error(inputElement.name, "p").classList.remove("front-error-active")
+     error(inputElement.name, "input").classList.remove("wrong-input")
+     error(inputElement.name, "label").classList.remove("wrong-label")
+     estado[inputElement.name]= true;
+     console.log(estado.name)
+}
+
+//Validador 9 blank description-------------------------------------------
+let blankDescription = (inputElement)=>{  
+        if(inputElement.value == ""){
+            //add error
+            error(inputElement.name, "small").classList.remove("front-blank-error-inactive")
+            error(inputElement.name, "small").classList.add("front-blank-error-active")
+            error(inputElement.name, "textarea").classList.add("wrong-input")
+            error(inputElement.name, "label").classList.add("wrong-label")
+            error(inputElement.name, "p").classList.add("front-error-inactive")
+            error(inputElement.name, "p").classList.remove("front-error-active")
+            backEndError.classList.add("front-blank-error-inactive")
+            estado[inputElement.name]= false;
+            return; 
+        }
+        //remove error
+        error(inputElement.name, "small").classList.add("front-blank-error-inactive")
+        error(inputElement.name, "small").classList.remove("front-blank-error-active")
+        error(inputElement.name, "textarea").classList.remove("wrong-input")
+        error(inputElement.name, "label").classList.remove("wrong-label")
+        estado[inputElement.name]= true;
+}
+
 //EJECUCION DE EVENTOS DE VALIDACION//
 
-//Validacion campo por campo (name y description)
+//Validacion campo por campo 
 inputs.forEach((input)=>{
     input.addEventListener("blur", (e)=>{
         validator(input, e);
@@ -160,17 +275,22 @@ inputs.forEach((input)=>{
 
 //Validacion imagen
 image.addEventListener("change", (e)=>{
-    /*3- Expresiones de imagen*/ return imgExtValidator(image);
+    if(image.value != ""){
+        /*3- Expresiones de imagen*/ return imgExtValidator(image);
+    }
 })
 
 //Validacion submit empty
 form.addEventListener("submit", (e)=>{
-    blankSubmitValidator(name);
-    validExpName(name, expresiones.nombre);
-    if(!estado.name){
+    if(!estado.name || !estado.precio || !estado.introduction || !estado.description || !estado.duration){
         e.preventDefault();
     }
-    console.table(estado)
+    inputs.forEach((input)=>{
+        if(input.name != "description"){
+            blankSubmitValidator(input);
+        }
+    })
+    blankDescription(description);
 })
 console.table(estado)
 
