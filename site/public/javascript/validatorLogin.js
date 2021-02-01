7751e5cca07d2e207f42f51fbb5869519fd7af7d
DMUGNOLO <islaedicion@iMac-de-IslaEdicion.local>
2021-02-01 19:11:13 -0300

    Validacion de login con api

--- site/public/javascript/validatorLogin.js
+++ site/public/javascript/validatorLogin.js
@@ -6,6 +6,8 @@
 - "front-blank-error-inactive" -- oculta el mensaje de campo en blanco.
 - "front-blank-error-active" -- activa el mensaje de campo en blanco. */
 
+const bcrypt = require("bcryptjs");
+
 /* Funcion que cambia las clases */
 let classController = (expresion,input)=>{
     if(expresion.test(input.value)){
@@ -16,7 +18,7 @@ let classController = (expresion,input)=>{
         document.querySelector(`.login-${input.name} small`).classList.remove("front-blank-error-active")
         document.querySelector(`.login-${input.name} small`).classList.add("front-blank-error-inactive")
         estados[input.name] = true
-    } else{
+    } else {
         document.querySelector(`.login-${input.name} p`).classList.add("front-error-active")
         document.querySelector(`.login-${input.name} p`).classList.remove("front-error-inactive")
         document.querySelector(`.login-${input.name} input`).classList.add("wrong-input")
@@ -26,7 +28,6 @@ let classController = (expresion,input)=>{
         estados[input.name] = false
     }
 }
-
 
 /* Funcion que chequea si los campos estan en blanco */
 let blankInput = (input)=>{
@@ -41,6 +42,49 @@ let blankInput = (input)=>{
     } 
 }
 
+/* Funcion que chequea si el usuario existe en la base de datos */
+async function userExist(input){
+    let apiUsers = await fetch("http://localhost:3000/api/users");
+    let usersResult = await apiUsers.json();
+    for (let user of usersResult.data){
+        console.log(user.password)
+        if(input.value != user.email){
+            console.log("usuario no esta registrado")
+            document.querySelector(`.login-${input.name} input`).classList.add("wrong-input")
+            document.querySelector(`.login-${input.name} label`).classList.add("wrong-label")
+            document.querySelector(`.login-${input.name} #user-exist-login-email-error`).classList.remove("front-error-inactive")
+            document.querySelector(`.login-${input.name} #user-exist-login-email-error`).classList.add("front-error-active")
+            estados[input.name] = false
+            return;
+        }
+        document.querySelector(`.login-${input.name} input`).classList.remove("wrong-input")
+        document.querySelector(`.login-${input.name} label`).classList.remove("wrong-label")
+        document.querySelector(`.login-${input.name} #user-exist-login-email-error`).classList.add("front-error-inactive")
+        document.querySelector(`.login-${input.name} #user-exist-login-email-error`).classList.remove("front-error-active")            
+        estados[input.name] = true
+        return;
+    }
+}
+
+async function passwordExist(input){
+    let apiUsers = await fetch("http://localhost:3000/api/users");
+    let usersResult = await apiUsers.json();
+    console.log(input.value)
+    for (let user of usersResult.data){
+        console.log(user.password)
+        if(!bcrypt.compareSync(input.value, user.password)){
+            console.log("Las contrasenias no coinciden")
+            estados[input.name] = false
+            return;    
+        }
+        estados[input.name] = true
+        return;    
+    }   
+}
+
+
+    
+  
 /* Expresiones */
 const expresiones = {
 	password: /^.{1,}$/, 
@@ -61,20 +105,25 @@ let estados={
 
 /* Identificando y validando los inputs */
 let validarCampos = (e)=>{
-    switch(e.target.name){
+    switch(e.target.name){  
         case "email":
             classController(expresiones.email,e.target)
             blankInput(e.target)
+            if(e.target.value != "" && expresiones.email.test(e.target.value)){
+                userExist(e.target)
+            }
         break;
         case "password":
             classController(expresiones.password,e.target)
             blankInput(e.target)
+            passwordExist(e.target)
         break;
     }
 }
 
 /* Eventos de los inputs */
 inputs.forEach((input)=>{
+    console.log(input)
     input.addEventListener("keyup",validarCampos)
     input.addEventListener("blur",validarCampos)
 })
