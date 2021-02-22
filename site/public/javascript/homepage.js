7b019ab45f39c89deefb793b7989a45a4a43c3d4
Agustin-morcillo <morcilloagustin95@gmail.com>
2021-02-22 17:44:56 -0300

    home-banner adjust

--- site/public/javascript/homepage.js
+++ site/public/javascript/homepage.js
@@ -16,3 +16,34 @@ setInterval(() => {
     }
 }, 4000);
 
+
+let vpWidth = window.innerWidth
+let images = document.querySelectorAll(".slide-img")
+let currentSrc = []
+
+images.forEach((image,i)=>{
+    currentSrc.push(image.src)
+})
+
+if(vpWidth<800){
+    images.forEach((image,i)=>{
+        image.src = `images/main/mobile_slider_${i+1}.jpg`
+    })
+}
+
+// Create a condition that targets viewports at least 768px wide
+const mediaQueryMobile = window.matchMedia('(max-width: 800px)')
+
+// Register event listener
+mediaQueryMobile.addListener((e)=>{
+    if(e.matches){
+        images.forEach((image,i)=>{
+            image.src = `images/main/mobile_slider_${i+1}.jpg`
+        })
+    } else{
+        for(let i=0;i<images.length;i++){
+            images[i].src = currentSrc[i]
+        }
+    }
+})
+
